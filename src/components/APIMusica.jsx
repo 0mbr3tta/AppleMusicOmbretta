f25a2a6643dc64d3bb530991469bc54519cf8aa5
0mbr3tta <0mbr3tta@gmail.com>
2025-01-24 16:59:25 +0100

    commit

--- src/components/APIMusica.jsx
+++ src/components/APIMusica.jsx
@@ -1,24 +1,44 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import { Container } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Card, Col, Row, Container } from 'react-bootstrap';
 
 function APIMusica() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=latest')
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.data) {
+          setAlbums(data.data); 
+        }
+      })
+      .catch((error) => console.error('Errore nel recuperare i dati:', error));
+  }, []);
+
   return (
-    <Container className="mt-2 mb-2 Sfondocard">
-        <h3 className="mt-5">Nuove uscite</h3>
-    <Row xs={3} md={3} className="g-4">
-      {Array.from({ length: 6 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <Container className="mt-2 mb-2 ">
+      <h3 className="mt-5">Nuove uscite</h3>
+      <Row xs={1} sm={2} md={3} className="g-4">
+        {albums.length > 0 ? (
+          albums.map((album, idx) => (
+            <Col key={idx}>
+              <Card>
+                <Card.Img className='immaginiApi'
+                  variant="top"
+                  src={album.album.cover_medium}
+                  alt={album.album.title}
+                />
+                <Card.Body>
+                  <Card.Title>{album.album.title}</Card.Title>
+                  <Card.Text>Artista: {album.artist.name}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <p>Caricamento album...</p>
+        )}
+      </Row>
     </Container>
   );
 }
--- src/components/ContentutoApple.jsx
+++ src/components/ContentutoApple.jsx
@@ -1,37 +1,40 @@
-import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap'; 
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 function ContenutoApple() {
-    return (
-        <Container>
-            <div>
-              <h1 className="mt-5">NOVITÀ</h1>
-              <hr></hr>
-            </div>
+  return (
+    <Container>
+      <div>
+        <h1 className="mt-5">NOVITÀ</h1>
+        <hr />
+      </div>
+
       <Row xs={2} md={2} className="g-4">
         <Col>
-          <Card className="Sfondocard">
+          <Card className="Sfondocards">
             <Card.Title>NOVITÀ STAZIONE RADIO</Card.Title>
             <Card.Text>
-             Rilassati al resto ci pensiamo noi. Ascolta apple Music Chill
+              Rilassati al resto ci pensiamo noi. Ascolta apple Music Chill
             </Card.Text>
             <Card.Img variant="top" src="/images/1a.png" />
           </Card>
         </Col>
         <Col>
-          <Card className="Sfondocard">
+          <Card className="Sfondocards">
             <Card.Title>NOVITÀ STAZIONE RADIO</Card.Title>
             <Card.Text>
-            Ecco la nuova casa della musica latina
+              Ecco la nuova casa della musica latina
             </Card.Text>
             <Card.Img variant="top" src="/images/1b.png" />
           </Card>
         </Col>
       </Row>
-<h3 className="mt-5 Sfondocard">Nuovi episodi radio</h3>
-      <Row xs={3} md={3} className="g-4">
+
+      <h3 className="mt-5 Sfondocard">Nuovi episodi radio</h3>
+
+      <Row xs={3} md={5} className="g-4">
         <Col>
           <Card className="Sfondocard">
             <Card.Img variant="top" src="/images/2a.png" />
@@ -40,20 +43,32 @@ function ContenutoApple() {
         </Col>
         <Col>
           <Card className="Sfondocard">
-            <Card.Img variant="top" src="/images/2b.png" /> 
+            <Card.Img variant="top" src="/images/2b.png" />
             <Card.Title>The wander</Card.Title>
           </Card>
         </Col>
         <Col>
           <Card className="Sfondocard">
-            <Card.Img variant="top" src="/images/2c.png" /> 
+            <Card.Img variant="top" src="/images/2c.png" className="dimensionimmagini w-100" />
             <Card.Title>Michael Bublè & Carly Pearace</Card.Title>
           </Card>
         </Col>
+        
+        <Col className="d-none d-sm-block">
+          <Card className="Sfondocard">
+            <Card.Img variant="top" src="/images/2d.png" />
+            <Card.Title>Stephan Moccio: The Zane Lowe Interview</Card.Title>
+          </Card>
+        </Col>
+        <Col className="d-none d-sm-block">
+          <Card className="Sfondocard">
+            <Card.Img variant="top" src="/images/2e.png" className='epradio' />
+            <Card.Title>Chart Spotlight: Julia Michaels</Card.Title>
+          </Card>
+        </Col>
       </Row>
-      </Container>
-     
-    );
-  }
+    </Container>
+  );
+}
 
 export default ContenutoApple;
