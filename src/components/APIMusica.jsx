8c7b8a6bdc2558d03adea1a26326f4f91c9e6118
0mbr3tta <0mbr3tta@gmail.com>
2025-01-24 17:08:43 +0100

    commit  finale davvero

--- src/components/APIMusica.jsx
+++ src/components/APIMusica.jsx
@@ -16,23 +16,23 @@ function APIMusica() {
   }, []);
 
   return (
-    <Container className="mt-2 mb-2  ">
+    <Container className="mt-2 mb-2">
       <h3 className="mt-5">Nuove uscite</h3>
-      <Row xs={5} sm={5} md={5} className="g-4 CardAPI">
+      <Row xs={5} sm={5} md={5} className="g-4">
         {albums.length > 0 ? (
-          albums.map((album, idx) => (
+          albums.slice(0, 10).map((album, idx) => ( // Mostra solo i primi 10 album
             <Col key={idx}>
-              <Card className='CardAPI'>
-                <Card.Img className='immaginiApi'
+              <Card className="CardAPI">
+                <Card.Img
+                  className="immaginiApi"
                   variant="top"
                   src={album.album.cover_medium}
                   alt={album.album.title}
                 />
-             <Card.Body>
-  <Card.Title className="fs-6">{album.album.title}</Card.Title> 
-  <Card.Text className="fs-7">Artista: {album.artist.name}</Card.Text> 
-</Card.Body>
-
+                <Card.Body>
+                  <Card.Title className="fs-6">{album.album.title}</Card.Title>
+                  <Card.Text className="fs-7">Artista: {album.artist.name}</Card.Text>
+                </Card.Body>
               </Card>
             </Col>
           ))
